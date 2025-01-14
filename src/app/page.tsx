f03bebacaa3f8f5ed24daca9697c84b93e093d9d
Marlon <mrlonbarretoborges@gmail.com>
2025-01-13 22:53:06 -0300

    feat: created abaut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import '../styles/animation.css';
 
 
 const techs = [
-  "Typescript",
+  "TypeScript",
   "JavaScript",
   "React",
   "Node.js",
@@ -22,49 +22,51 @@ const techs = [
 export default function Home() {
   return (
     <div className="flex flex-col h-screen items-center">
-      <div className=" text-white  md:w-5/6">
-        <section className="flex flex-col md:flex-row items-center justify-start h-3/6 mt-10 rounded-md font-mono p-4 gap-5">
+      <div className=" text-white  md:w-4/6">
+        <section className="flex flex-col md:flex-row items-center justify-start mt-10 rounded-md font-mono p-4 gap-5">
         <Image src="https://avatars.githubusercontent.com/u/163504930?v=4" alt="foto de perfil" width={200} height={200} className="rounded-full border-2 border-zinc-500"/>
-          <div className="flex flex-col w-full h-full gap-3">
+          <div className="flex flex-col w-full gap-3">
             <h1 className=" text-2xl font-bold md:text-4xl w-full text-center md:text-start">Desenvolvedor Fullstack</h1>
             <div className="flex flex-col gap-4">
               <ul className="flex flex-wrap gap-2 mt-5 md:justify-start justify-center">
                 {techs.map((tech) => (
-                  <li key={tech} className=" text-sm border-2 border-zinc-700 px-2 font-extrabold bg-zinc-700 rounded-xl">{tech}</li>
+                  <li key={tech} className=" text-xs border-2 border-zinc-700 px-2.5 py-0.5 font-semibold bg-zinc-700 rounded-full">{tech}</li>
                 ))}
               </ul>
             </div>
-            <div className="txt-animado">
-              <span></span>
-            </div>  
             <div className="flex gap-4 border-zinc-500 justify-center md:justify-start"> 
-              <Link href="https://github.com/MarlonBBo">
+              <Link href="https://github.com/MarlonBBo" target="_blank" rel="noopener noreferrer">
                 <div className="border-2 border-zinc-700 p-2 rounded-xl hover:bg-zinc-700 transition duration-500">
                   <FaGithub className="w-6 h-6"/>
                 </div>
               </Link>
-              <Link href="https://www.linkedin.com/in/marlonbborges1606/">
+              <Link href="https://www.linkedin.com/in/marlonbborges1606/ "target="_blank" rel="noopener noreferrer">
                 <div className="border-2 border-zinc-700 p-2 rounded-xl hover:bg-zinc-700 transition duration-500">
                   <CiLinkedin className="w-6 h-6"/>
                 </div>
               </Link>
-              <Link href="https://github.com/MarlonBBo">
+              <Link href="#">
                 <div className="border-2 border-zinc-700 p-2 rounded-xl hover:bg-zinc-700 transition duration-500">
                   <MdEmail className="w-6 h-6"/>
                 </div>
               </Link>
-              <Link href="https://github.com/MarlonBBo">
+              <Link href="#">
                 <div className="flex gap-2 border-2 border-zinc-700 p-2 rounded-xl hover:bg-zinc-700 transition duration-500">
                   <IoIosPaper className="w-6 h-6"/> <p className="text-md font-bold">Download</p>
                 </div>
-              </Link>
+              </Link> 
             </div>
+            <div className="txt-animado">
+                <span></span>
+            </div> 
           </div>
         </section>
 
-        <section className="flex flex-col items-center justify-center h-2/5 m-10 border-2  rounded-md font-mono  p-4">
-          <h1 className="text-4xl font-mono">My Profile</h1>
-          <p className="text-lg font-mono">Bem vindo ao meu perfil</p>
+        <section className="flex flex-col m-10 border border-zinc-700 gap-4 rounded-md p-4">
+          <h1 className="text-2xl font-bold">Sobre mim</h1>
+          <p className="text-lg font-sans text-zinc-400">Sou Marlon Barreto Borges, apaixonado por tecnologia e desafios. Busco me desenvolver através de desafios usando minhas habilidades e crescer como desenvolvedor. Possuo excelentes habilidades de resolução de problemas, trabalho em equipe e criatividade.  
+            Iniciei na programação querendo criar meus próprios sites e apps, o que me fez desenvolver uma abordagem criativa e proativa para resolver desafios. Sou uma pessoa bem-humorada que busca constantemente maneiras de criar um ambiente colaborativo e divertido.
+            Atualmente desenvolvo minhas aplicações com <strong>Javascript</strong>, uso tecnologias como: <strong>Typescript, React, Tailwindcss, NodeJS, Express, NestJS, Prisma, PostgresSQL, Docker, Git</strong></p>
         </section>
 
         <section className="flex flex-col items-center justify-center h-2/5 m-10 border-2 rounded-md font-mono  p-4">
