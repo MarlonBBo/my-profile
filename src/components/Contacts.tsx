834be9decb961c70d2a337286950e5aa8c3c9eeb
Marlon <mrlonbarretoborges@gmail.com>
2025-01-16 02:11:08 -0300

    feat: navegação para email

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -14,12 +14,12 @@ export function Contacts(){
                   <RiGithubLine className="w-6 h-6 text-black dark:text-white"/>
                 </div>
               </Link>
-              <Link href="https://www.linkedin.com/in/marlonbborges1606/ "target="_blank" rel="noopener noreferrer">
+              <Link href="https://www.linkedin.com/in/marlonbborges1606/ "target="_blank">
                 <div className="border border-zinc-700 dark:hover:bg-zinc-700 p-2 rounded-xl hover:bg-zinc-200 transition duration-500">
                   <FiLinkedin className="w-6 h-6 text-black dark:text-white"/>
                 </div>
               </Link>
-              <Link href="#">
+              <Link href="mailto:contato.marlonbarretoborges@gmail.com" target="_blank">
                 <div className="border border-zinc-700 dark:hover:bg-zinc-700 p-2 rounded-xl hover:bg-zinc-200 transition duration-500">
                   <MdOutlineEmail className="w-6 h-6 text-black dark:text-white"/>
                 </div>
